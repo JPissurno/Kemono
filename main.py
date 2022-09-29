1319aa7f161e1505d3edfc6240cc5559b65c5431
Joao Pissurno <joaovpissurno@gmail.com>
2022-09-28 21:30:18 -0300

    tst

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ args = {}
 BASE_URL = 'https://kemono.party'
 BASE_URL_ARTIST = 'https://kemono.party/artists/'
 
-SIMILAR_ARTIST_NAME_RATIO = 0.90
+SIMILAR_ARTIST_NAME_RATIO = 0.90  # testing
 EXTENSIONS_VIDEO = ['.mp4', '.avi', '.mkv', '.webm']
 EXTENSIONS_IMAGE = ['.jpg', '.jpeg', '.png', '.jpe', '.webp', '.gif']
 
